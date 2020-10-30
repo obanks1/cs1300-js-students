1bc1b79999af5b679568d3934590aa1a004d010a
bankso <olivia_banks@brown.edu>
2020-10-29 21:26:19 -0400

    plants you can fit in your pocket

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=sPWqqd35MRckbSGu8txSnzvA0N7hAiKZVFaObwNXB70";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -15,7 +15,7 @@ const doCORSRequest = (options) => {
 const corsPromise = () =>
   new Promise((resolve, reject) => {
     const request = doCORSRequest({
-      url: "https://trefle.io/api/v1/plants" + apiToken,
+      url: "https://trefle.io/api/v1/species" + apiToken + "&range[maximum_height_cm]=,8",
     });
     resolve(request);
   });
@@ -25,7 +25,32 @@ corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
       // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+      const plantData = JSON.parse(request.response).data;
+      console.log(plantData);
+      updatePage(plantData)
+
     })
 );
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+const updatePage = (plantData) => {
+  plantData.forEach(plant => {
+    createUserCard(plant);
+  });
+}
+
+const createUserCard = (plant) => {
+  let card = document.createElement('div');
+  card.className = "card";
+  let name = document.createElement('h5');
+  let scientificName = document.createElement('h6');
+  let image = document.createElement('img')
+  name.innerHTML = plant.common_name;
+  scientificName.innerHTML = plant.scientific_name;
+  image.src = plant.image_url;
+  card.appendChild(name);
+  card.appendChild(scientificName);
+  card.appendChild(image);
+
+  document.getElementById('plantList').appendChild(card);
+}
